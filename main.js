f8983f3ccf2b3c98bae33c14fd5173cdae71eda9
Wate02 <123736231+Wate02@users.noreply.github.com>
2023-11-28 17:36:18 +0000

    deleted:    Html1.zip 	new file:   img/Emu.png 	new file:   img/Sub.png 	new file:   img/clip.png 	new file:   img/ctrtt.png 	new file:   img/eyti.png 	new file:   img/mario64webgl.png 	new file:   index.html 	new file:   main.js     new file:   style.css 	new file:   tabs/beta.html 	new file:   tabs/games.html 	new file:   tabs/tools.html

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log('Hello World!');
+
+function eytiapp2() {
+  location.href = 'https://wate02.github.io/Mario64webgl/';
+}
+
+function sub() {
+    location.href = 'https://wate02.github.io/Idk2/html5-games/games/subway-surfers-unity/';
+}
+
+function emu() {
+    location.href = 'https://wate02.github.io/EmulatorJS//';
+}
+
+
+function imgtoclipboard() {
+    location.href = 'https://wate02.github.io/Imgtoclipboard/';
+}
+
+function ctrtt() {
+    location.href = 'https://wate02.github.io/Idk2/html5-games/games/ctr-TimeTravel/';
+}
+
