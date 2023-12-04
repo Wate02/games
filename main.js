7edd2915c3b84a7f2409649a0eb37a6004efb87a
alx.schule@gmail.com <alx.schule@gmail.com>
2023-12-04 19:48:38 +0100

    added darkmode

--- main.js
+++ main.js
@@ -28,3 +28,46 @@ function esurf() {
   location.href = 'https://wate02.github.io/Idk2/html5-games/games/edge-surf/';
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Dark Mode
+
+const darkModeEnabled = localStorage.getItem('darkMode') === 'enabled';
+
+
+function enableDarkMode() {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+
+function disableDarkMode() {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', null);
+}
+
+
+if (darkModeEnabled) {
+    enableDarkMode();
+}
+
+
+document.getElementById('toggleBtn').addEventListener('click', () => {
+    if (document.body.classList.contains('dark-mode')) {
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+});
