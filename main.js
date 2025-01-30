348fa7f64ad9a1ae923110bd5def8407f8d5e778
Wate02 <alx.schule@gmail.com>
2025-01-30 12:57:04 +0100

    update

--- main.js
+++ main.js
@@ -92,6 +92,14 @@ function domr() {
 function domc() {
   location.href = 'https://wate02.github.io/BrowserGames/DayOfMeat/DayOfMeatCastle/'
 }
+
+function fnaf2() {
+  location.href = 'https://wate02.github.io/FNAF2HTML'
+}
+
+function fnaf1() {
+  location.href = 'https://wate02.github.io/FNAF1HTML'
+}
 //<div id="yellowinfocard"><img id="iicon"src="/img/i.png" height="20px"> wate02.github.io kann jetzt als Web-app auf IOS verwendet werden. Clicke dafür auf <img id="shareicon"src="/img/share.png" height="20px" width="15px"> und dann auf "Zum Startbildschirm hinzufügen".
 //<style> 
 //#shareicon {
